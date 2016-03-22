29b6bd7074788a138b9a5355df30d38a470047d3
dzzie <dzzie@yahoo.com>
2016-03-21 20:14:32 -0400

    +walk collection values in c

--- vb6_collections_in_c/use_vb6_collection_from_c/col.cpp
+++ vb6_collections_in_c/use_vb6_collection_from_c/col.cpp
@@ -5,7 +5,7 @@
 
 //#import "C:\\windows\system32\msvbvm60.dll" no_namespace
 
-#import "c:\Project1.dll" 
+#import "Project1.dll" 
 
 Project1::_Class1Ptr c;
 
@@ -13,11 +13,14 @@ void addStr(_CollectionPtr p , char* str){
 	_variant_t vv;
 	vv.SetString(str);
 	p->Add( &vv.GetVARIANT() );
+}
 
-	/*VARIANT v;
-	VariantInit(&v);
-	v.bstrVal = SysAllocString(L"this is string 1!");
-	v.vt = VT_BSTR;*/
+void addInt(_CollectionPtr p , int x){
+	_variant_t vv;// = x; //this gave error automation type not supported in visual basic..
+						  //see comutil.h it was making it an VT_INT, a long -> VT_I4
+	vv.vt = VT_I4;        //I will just set it manually or can change fun prototype or cast value..
+	vv.intVal  = x;
+	p->Add( &vv.GetVARIANT() );
 }
 
 void main(void)
@@ -40,34 +43,35 @@ void main(void)
 
 	pColl->Add(&v);
 
-	VARIANT v2;
-	VariantInit(&v2);
-	v2.bstrVal = SysAllocString(L"this is string 2!");
-	v2.vt = VT_BSTR;
-
-	pColl->Add(&v2);
-
+	addInt(pColl,47);
 	addStr(pColl, "this is my wrapper!");
 	addStr(pColl, "this is my wrapper1");
 	addStr(pColl, "this is my wrapper2");
 	addStr(pColl, "this is my wrapper3");
-	 
+	
+	//remember vb collections start at 1
+	for(long i=1; i <= pColl->Count(); i++){
+		_variant_t index = i;
+		_variant_t vTmp = pColl->Item(&index);
+
+		//automatically converts (some) types to strings for you
+		//I think I read this can throw an exception in some cases..(VT_UNKNOWN?)
+		_bstr_t b = vTmp; 
+
+		printf("Item %d is type.vt=%d  CStr('%s')\n",i, vTmp.vt, (char*)b);
+	}
+
 	c->test();
 
 	 
 
-	/*HRESULT hr = pColl.CreateInstance(clsid);
+	/* vb6 collection is not creatable externally via clsid apparently..
+	HRESULT hr = pColl.CreateInstance(clsid);
 	if(FAILED(hr) )
 	{
-	MessageBox(0,"_Collection:CreateInstance:Failed","CSvcEventWrapper",0);
+		MessageBox(0,"_Collection:CreateInstance:Failed","Oops...",0);
 	}*/
 
-
-     getch();
-
-
-
- 
- 
-
+    printf("\nPress any key to exit...");
+    getch();
 }
