995a27291764ad22009bc97a73109642319bff53
dzzie <dzzie@yahoo.com>
2020-09-14 23:04:26 -0400

    dll hell recovery (sppe3/vbutypes) - flubbed in IDA JScript 2 push, broke MAP.ShellExt and YaraWorkbench

--- pe_lib3/_copy.bat
+++ pe_lib3/_copy.bat
@@ -1,2 +1,3 @@
-copy sppe3.dll D:\_code\iDef\MAP\dependancies\
+copy sppe3.dll D:\_code\MAP\dependancies\
+copy sppe3.dll d:\_code\vbYara\yara_workbench\dependancies
 pause
--- vb6_utypes/_copy.bat
+++ vb6_utypes/_copy.bat
@@ -1,3 +1,6 @@
-copy UTypes.dll D:\_code\iDef\MAP\dependancies\
-copy vbUTypes.dll D:\_code\iDef\MAP\dependancies\
+copy UTypes.dll D:\_code\MAP\dependancies\
+copy vbUTypes.dll D:\_code\MAP\dependancies\
+copy UTypes.dll d:\_code\vbYara\yara_workbench\dependancies\
+copy vbUTypes.dll d:\_code\vbYara\yara_workbench\dependancies\
+copy UTypes.dll D:\_code\RE_Plugins\IDA_JScript_w_DukDbg\binary_snapshot\
 pause
